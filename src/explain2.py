6d972f946c9bab9257c55b705c02e5bd6b4758ea
stoir <sanjucheerla@gmail.com>
2023-04-12 20:45:14 -0400

    implemented binary search in firstN function

--- src/explain2.py
+++ src/explain2.py
@@ -1,5 +1,4 @@
 
- 
 
 from typing import List, Dict, Tuple
 from util import rnd
@@ -9,6 +8,8 @@ import time
 from data import Data
 from discretization import value, bins, RANGE
 from string_util import *
+
+
 class Explain2:
     def __init__(self, best: Data, rest: Data):
         self.best = best
@@ -26,22 +27,25 @@ class Explain2:
 
             if len(bestr) + len(restr) > 0:
                 return value({"best": len(bestr), "rest": len(restr)}, len(self.best.rows), len(self.rest.rows), "best"), rule
-        return None,None
+        return None, None
 
     def xpln2(self, data: Data, best: Data, rest: Data):
         start_time = time.time()
+
         def v(has):
             return value(has, len(best.rows), len(rest.rows), "best")
-        
-        tmp,self.max_sizes = [],{}
-        for _,ranges in enumerate(bins(data.cols.x,{"best":best.rows, "rest":rest.rows})):
+
+        tmp, self.max_sizes = [], {}
+        for _, ranges in enumerate(bins(data.cols.x, {"best": best.rows, "rest": rest.rows})):
             self.max_sizes[ranges[0]['txt']] = len(ranges)
-            for _,range in enumerate(ranges):
-                tmp.append({"range":range, "max":len(ranges),"val": v(range['y'].has)})
-        rule, most = self.firstN(heapq.nlargest(len(ranges), tmp, key=lambda x: x["val"]), self.score)
+            for _, range in enumerate(ranges):
+                tmp.append({"range": range, "max": len(
+                    ranges), "val": v(range['y'].has)})
+        rule, most = self.firstN(heapq.nlargest(
+            len(ranges), tmp, key=lambda x: x["val"]), self.score)
         print("time xpln2= " + str(time.time()-start_time))
-        return rule,most
-    
+        return rule, most
+
     def firstN(self, sorted_ranges, scoreFun):
         first = sorted_ranges[0]['val']
 
@@ -52,41 +56,55 @@ class Explain2:
         most: int = -1
         out: int = -1
 
-        for n in range(len(sorted_ranges)):
+        # perform binary search to find the range of indices that produce a positive score
+        lo, hi = 0, len(sorted_ranges) - 1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            tmp, rule = scoreFun([r['range'] for r in sorted_ranges[:mid+1]])
+            if tmp is None or tmp <= 0:
+                lo = mid + 1
+            else:
+                hi = mid - 1
+
+        # iterate over the range of indices to find the optimal value of n
+        for n in range(lo, len(sorted_ranges)):
             tmp, rule = scoreFun([r['range'] for r in sorted_ranges[:n+1]])
 
             if tmp is not None and tmp > most:
                 out, most = rule, tmp
 
         return out, most
-    
-    def rule(self, ranges,maxSize):
-        t={}
-        for _,range in enumerate(ranges):
+
+    def rule(self, ranges, maxSize):
+        t = {}
+        for _, range in enumerate(ranges):
             t[range['txt']] = t.get(range['txt'], [])
-            t[range['txt']].append({"lo":range['lo'],"hi":range['hi'],"at":range['at']})
+            t[range['txt']].append(
+                {"lo": range['lo'], "hi": range['hi'], "at": range['at']})
         return self.prune(t, maxSize)
 
     def prune(self, rule, maxSize):
-        n=0
+        n = 0
         new_rule = {}
-        for txt,ranges in rule.items():
+        for txt, ranges in rule.items():
             n = n+1
             if len(ranges) == maxSize[txt]:
-                n=n-1
+                n = n-1
                 rule[txt] = None
             else:
                 new_rule[txt] = ranges
-        if n > 0: 
+        if n > 0:
             return new_rule
         return None
 
-def showRule(self,rule):
+
+def showRule(self, rule):
     def pretty(range):
-        return range['lo'] if range['lo']==range['hi'] else [range['lo'], range['hi']]
+        return range['lo'] if range['lo'] == range['hi'] else [range['lo'], range['hi']]
+
     def merge(t0):
-        t,j =[],1
-        while j<=len(t0):
+        t, j = [], 1
+        while j <= len(t0):
             left = t0[j-1]
             if j < len(t0):
                 right = t0[j]
@@ -94,10 +112,11 @@ def showRule(self,rule):
                 right = None
             if right and left['hi'] == right['lo']:
                 left['hi'] = right['hi']
-                j=j+1
-            t.append({'lo':left['lo'], 'hi':left['hi']})
-            j=j+1
-        return t if len(t0)==len(t) else merge(t) 
+                j = j+1
+            t.append({'lo': left['lo'], 'hi': left['hi']})
+            j = j+1
+        return t if len(t0) == len(t) else merge(t)
+
 
 def selects(rule, rows):
     def disjunction(ranges, row):
@@ -105,18 +124,20 @@ def selects(rule, rows):
             at = rang['at']
             x = row.cells[at]
             lo = rang['lo']
-            hi = rang['hi']  
-            if x == '?' or (lo == hi and lo == x) or (lo <= x and x< hi):
+            hi = rang['hi']
+            if x == '?' or (lo == hi and lo == x) or (lo <= x and x < hi):
                 return True
         return False
+
     def conjunction(row):
-        for _,ranges in rule.items():
+        for _, ranges in rule.items():
             if not disjunction(ranges, row):
                 return False
         return True
+
     def function(r):
         return r if conjunction(r) else None
-    
+
     r = []
     for item in list(map(function, rows)):
         if item:
